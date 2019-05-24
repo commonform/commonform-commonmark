b7948bf4d144563c32e8da00fc3f8755d41d44ae
Kyle E. Mitchell <kyle@kemitchell.com>
2019-05-24 10:40:40 -0700

    Rewrite bin script

--- bin.js
+++ bin.js
@@ -1,21 +1,73 @@
 #!/usr/bin/env node
-var chunks = []
-process.stdin
-  .on('data', function (chunk) {
-    chunks.push(chunk)
-  })
-  .once('error', function (error) {
-    console.error(error)
-    process.exit(1)
-  })
-  .once('end', function () {
-    var input = Buffer.concat(chunks).toString()
-    var commonmark = require('./')
-    try {
-      var form = commonmark.parse(input)
-    } catch (error) {
-      console.error(error)
-      process.exit(1)
+require('yargs') // eslint-disable-line
+  .scriptName('commonform-commonmark')
+
+  .command(
+    'parse',
+    'parse CommonMark to Common Form',
+    function (args) {
+      readInput(function (buffer) {
+        try {
+          var parsed = require('./').parse(buffer.toString())
+        } catch (error) {
+          fail(error)
+        }
+        console.log(JSON.stringify(parsed))
+        process.exit(0)
+      })
     }
-    console.log(JSON.stringify(form))
-  })
+  )
+
+  .command(
+    'stringify',
+    'stringify Common Form JSON to CommonMark',
+    function (yargs) {
+      return yargs.option('b', {
+        alias: 'blanks',
+        describe: 'JSON files with fill-in-the-blank values'
+      })
+    },
+    function (argv) {
+      readInput(function (input) {
+        var blanks
+        if (argv.blanks) {
+          try {
+            blanks = JSON.parse(require('fs').readFileSync(argv.blanks))
+          } catch (error) {
+            fail(error)
+          }
+        } else {
+          blanks = {}
+        }
+        try {
+          var json = JSON.parse(input)
+          var stringified = require('./').stringify(json, blanks, {})
+        } catch (error) {
+          fail(error)
+        }
+        console.log(stringified)
+        process.exit(0)
+      })
+    }
+  )
+
+  .version()
+  .help()
+  .alias('h', 'help')
+  .demandCommand()
+  .argv
+
+function readInput (callback) {
+  require('simple-concat')(
+    process.stdin,
+    function (error, buffer) {
+      if (error) fail(error)
+      callback(buffer)
+    }
+  )
+}
+
+function fail (error) {
+  console.error(error)
+  process.exit(1)
+}
